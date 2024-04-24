ec4308fad1a071bf4462517e7012245a583c6aa5
lamnhan066 <hello@lamnhan.dev>
2024-04-24 16:47:27 +0700

    Remove unused methods in the `IsolateManagerController`

--- lib/src/isolate_manager_controller.dart
+++ lib/src/isolate_manager_controller.dart
@@ -1,9 +1,8 @@
 import 'package:isolate_contactor/isolate_contactor.dart';
 
-class IsolateManagerController<R, P>
-    implements IsolateContactorController<R, P> {
+class IsolateManagerController<R, P> {
   /// Delegation of IsolateContactor
-  final IsolateContactorController<R, P> _delegate;
+  late IsolateContactorController<R, P> _delegate;
 
   /// This method only use to create your own isolate.
   ///
@@ -11,44 +10,27 @@ class IsolateManagerController<R, P>
   /// `onDispose` will be called when the controller is disposed.
   IsolateManagerController(
     dynamic params, {
-    Function()? onDispose,
-  }) : _delegate =
-            IsolateContactorController<R, P>(params, onDispose: onDispose);
+    Function(IsolateManagerController controller)? onDispose,
+  }) {
+    _delegate = IsolateContactorController<R, P>(
+      params,
+      onDispose: onDispose == null ? null : () => onDispose(this),
+    );
+  }
 
   /// Close this `IsolateManagerController`
-  @override
   Future<void> close() => _delegate.close();
 
-  /// Get current controller for IsolateManager
-  @override
-  get controller => _delegate.controller;
-
   /// Get initial parameters when you create the IsolateManager
-  @override
   get initialParams => _delegate.initialParams;
 
   /// This parameter is only used for Isolate. Use to listen for values from the main application.
-  @override
   Stream<P> get onIsolateMessage => _delegate.onIsolateMessage;
 
-  /// This parameter is only used by the main application. Use to listen for values from Isolate.
-  @override
-  Stream<R> get onMessage => _delegate.onMessage;
-
-  /// Send values from the main app to Isolate for computation (to `onIsolateMessage`).
-  @override
-  void sendIsolate(P message) => _delegate.sendIsolate(message);
-
-  /// Send a `Dispose` message to the isolate
-  @override
-  void sendIsolateState(Object state) => _delegate.sendIsolateState(state);
-
   /// Send values from Isolate to the main application (to `onMessage`).
-  @override
   void sendResult(R result) => _delegate.sendResult(result);
 
   /// Send the `Exception` to the main app.
-  @override
   void sendResultError(IsolateException exception) =>
       _delegate.sendResultError(exception);
 }
