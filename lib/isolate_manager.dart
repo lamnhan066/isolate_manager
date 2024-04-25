2daeb98878729b87baca432220af9880ae718f00
lamnhan066 <hello@lamnhan.dev>
2024-04-25 12:33:21 +0700

    Export the `IsolateFunction`

--- lib/isolate_manager.dart
+++ lib/isolate_manager.dart
@@ -2,6 +2,7 @@ library isolate_manager;
 
 export 'package:isolate_contactor/isolate_contactor.dart' show IsolateException;
 
+export 'src/isolate_function.dart';
 export 'src/isolate_manager.dart';
 export 'src/isolate_manager_controller.dart';
 export 'src/isolate_worker/isolate_worker.dart';
