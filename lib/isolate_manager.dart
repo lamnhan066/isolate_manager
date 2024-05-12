21965b915ec8ef8899276085f3b78e6856c3728d
lamnhan066 <hello@lamnhan.dev>
2024-05-12 23:58:17 +0700

    Export the `IsolateState`.

--- lib/isolate_manager.dart
+++ lib/isolate_manager.dart
@@ -1,7 +1,7 @@
 library isolate_manager;
 
 export 'src/base/isolate_contactor.dart'
-    show IsolateException, IsolateConverter, IsolateFunction;
+    show IsolateException, IsolateConverter, IsolateFunction, IsolateState;
 export 'src/isolate_manager.dart';
 export 'src/isolate_manager_controller.dart';
 export 'src/isolate_manager_function.dart';
--- lib/src/base/isolate_contactor.dart
+++ lib/src/base/isolate_contactor.dart
@@ -3,3 +3,4 @@ library isolate_contactor;
 export 'src/isolate_contactor.dart';
 export 'src/isolate_contactor_controller.dart';
 export 'src/models/exception.dart';
+export 'src/models/isolate_state.dart';
