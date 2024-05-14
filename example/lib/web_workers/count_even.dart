c3f98e8069e66627f67512967aa764ba36ec0509
lamnhan066 <hello@lamnhan.dev>
2024-05-14 12:51:00 +0700

    Remove the `web_workers` folder in favor of the generator

--- example/lib/web_workers/count_even.dart
+++
@@ -1,8 +0,0 @@
-import 'package:isolate_manager/isolate_manager.dart';
-import 'package:isolate_manager_example/functions.dart';
-
-/// dart compile js count_even.dart -o count_even.js -O4
-
-void main(List<String> args) {
-  IsolateManagerFunction.workerFunction(countEven);
-}
