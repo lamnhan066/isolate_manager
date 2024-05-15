df699412313820c2bdbc5e445fc10a855103b85b
lamnhan066 <hello@lamnhan.dev>
2024-05-15 22:07:12 +0700

    Add comment to the library

--- lib/isolate_manager.dart
+++ lib/isolate_manager.dart
@@ -1,8 +1,9 @@
+/// Create multiple long-lived isolates for a function (keep it active to send and receive data),
+/// supports Worker and WASM on the Web.
 library isolate_manager;
 
 export 'src/base/isolate_contactor.dart'
     show IsolateException, IsolateConverter, IsolateFunction, IsolateState;
-
 export 'src/isolate_manager.dart';
 export 'src/isolate_manager_controller.dart';
 export 'src/isolate_manager_function.dart';
