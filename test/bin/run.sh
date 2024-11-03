4bab6224d5341e33bc04ead6d5a3f8233a348745
lamnhan066 <hello@lamnhan.dev>
2024-11-03 09:07:16 +0700

    Move `run.sh` to `test/bin`

--- test/bin/run.sh
+++ test/bin/run.sh
@@ -11,5 +11,4 @@ dart test --platform=vm --coverage=coverage
 
 dart run coverage:format_coverage --lcov --in=coverage --out=coverage/lcov.info --packages=.dart_tool/package_config.json --report-on=lib --check-ignore
 
-# sed -i 's|SF:.*/lib|SF:lib|' coverage/lcov.info
-sed -i '' 's|SF:.*/lib|SF:lib|' coverage/lcov.info 
+sed -i 's|SF:.*/lib|SF:lib|' coverage/lcov.info
