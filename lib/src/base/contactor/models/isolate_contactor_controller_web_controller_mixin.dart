c95ee5d6a7d960d5b29178c1e7d5c5bcb2145c04
lamnhan066 <hello@lamnhan.dev>
2025-03-03 09:51:02 +0700

    Remove unused `UnimplementedError`

--- lib/src/base/contactor/models/isolate_contactor_controller_web_controller_mixin.dart
+++ lib/src/base/contactor/models/isolate_contactor_controller_web_controller_mixin.dart
@@ -3,5 +3,5 @@
 mixin IsolateContactorControllerWebControllerMixin {
   /// This controller is only for web that will be sent to the Worker
   /// or a Future.
-  Object get controller => UnimplementedError();
+  Object get controller;
 }
