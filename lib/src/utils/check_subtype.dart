6716be309aa708358a13c26a0681a294c2eb04fb
lamnhan066 <hello@lamnhan.dev>
2025-03-05 17:10:17 +0700

    Add isSubtype utility function for generic subtype checking

--- lib/src/utils/check_subtype.dart
+++ lib/src/utils/check_subtype.dart
@@ -1,4 +1,7 @@
 import 'package:isolate_manager/isolate_manager.dart';
 
+/// Check if [S] is subtype of [T]
+bool isSubtype<S, T>() => <S>[] is List<T>;
+
 /// Check if [S] is subtype of [ImType]
-bool isIsolateTypeSubtype<S>() => <S>[] is List<ImType>;
+bool isIsolateTypeSubtype<S>() => isSubtype<S, ImType>();
