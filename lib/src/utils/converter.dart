a02ebf5c06252b10ea467b85665df28824e377bc
lamnhan066 <hello@lamnhan.dev>
2025-03-05 18:11:24 +0700

    Enhance converterHelper to support custom converters for numeric value transformations in WebAssembly

--- lib/src/utils/converter.dart
+++ lib/src/utils/converter.dart
@@ -1,18 +1,19 @@
+import 'package:isolate_manager/isolate_manager.dart';
 import 'package:isolate_manager/src/utils/check_subtype.dart';
 
 /// A constant that is true if the application was compiled to WebAssembly.
 const bool kIsWasm = bool.fromEnvironment('dart.tool.dart2wasm');
 
 /// Converts [value] to type [R], handling numeric conversions for WASM.
-R converterHelper<R>(dynamic value) {
+R converterHelper<R>(dynamic value, [IsolateConverter<R>? customConverter]) {
+  dynamic effectiveValue = value;
   if (kIsWasm) {
-    if (isSubtype<R, int>()) return (value as num).toInt() as R;
-    if (isSubtype<R, Iterable<int>>()) {
-      return (value as Iterable).cast<num>().map((e) => e.toInt()) as R;
-    }
-    if (isSubtype<R, List<int>>()) {
-      return (value as List).cast<num>().map((e) => e.toInt()).toList() as R;
+    if (isSubtype<R, int>()) {
+      effectiveValue = (effectiveValue as num).toInt() as R;
+    } else if (isSubtype<R, Iterable<int>>()) {
+      effectiveValue =
+          (effectiveValue as Iterable).cast<num>().map((e) => e.toInt()) as R;
     }
   }
-  return value as R;
+  return customConverter?.call(effectiveValue) ?? effectiveValue as R;
 }
