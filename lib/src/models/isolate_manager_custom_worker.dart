2b002b452962ce4ca007ab9dcee3a0468634700b
Lam Thanh Nhan <hello@lamnhan.dev>
2025-09-12 14:51:15 +0700

    Refactor IsolateManagerWorker and IsolateManagerCustomWorker classes to use private naming convention

--- lib/src/models/isolate_manager_custom_worker.dart
+++ lib/src/models/isolate_manager_custom_worker.dart
@@ -1,33 +1,8 @@
 /// An annotation that you want to use for the custom function.
-const isolateManagerCustomWorker = IsolateManagerCustomWorker();
+const isolateManagerCustomWorker = _IsolateManagerCustomWorker();
 
 /// The annotation to generate a Worker JS for a specific method.
-class IsolateManagerCustomWorker {
+class _IsolateManagerCustomWorker {
   /// The annotation to generate a custom Worker JS for a specific method.
-  ///
-  /// If the [name] is specified, the Worker JS will be named `<name>.js`, otherwise
-  /// the name of the function will be used.
-  ///
-  /// If you want to put the Worker JS inside a subfolder, you can use `subfolder/name` pattern.
-  ///
-  /// For instance:
-  ///
-  /// ```dart
-  /// @IsolateManagerCustomWorker('workers/add')
-  /// int add(List<int> params) {
-  ///   return params[0] + params[1];
-  /// }
-  /// ```
-  ///
-  /// The generated path will be:
-  ///
-  /// ```console
-  /// web
-  /// |-- workers
-  /// |-- |-- add.js
-  /// ```
-  const IsolateManagerCustomWorker([this.name = '']);
-
-  /// Name of the Worker JS.
-  final String name;
+  const _IsolateManagerCustomWorker();
 }
--- lib/src/models/isolate_manager_worker.dart
+++ lib/src/models/isolate_manager_worker.dart
@@ -1,33 +1,8 @@
-/// A default annotation for the [IsolateManagerWorker] with empty name.
-const IsolateManagerWorker isolateManagerWorker = IsolateManagerWorker();
+/// A default annotation for the [_IsolateManagerWorker] with empty name.
+const isolateManagerWorker = _IsolateManagerWorker();
 
 /// The annotation to generate a Worker JS for a specific method.
-class IsolateManagerWorker {
+class _IsolateManagerWorker {
   /// The annotation to generate a Worker JS for a specific method.
-  ///
-  /// If the [name] is specified, the Worker JS will be named `<name>.js`, otherwise
-  /// the name of the function will be used.
-  ///
-  /// If you want to put the Worker JS inside a subfolder, you can use `subfolder/name` pattern.
-  ///
-  /// For instance:
-  ///
-  /// ```dart
-  /// @IsolateManagerWorker('workers/add')
-  /// int add(List<int> params) {
-  ///   return params[0] + params[1];
-  /// }
-  /// ```
-  ///
-  /// The generated path will be:
-  ///
-  /// ```console
-  /// web
-  /// |-- workers
-  /// |-- |-- add.js
-  /// ```
-  const IsolateManagerWorker([this.name = '']);
-
-  /// Name of the Worker JS.
-  final String name;
+  const _IsolateManagerWorker();
 }
